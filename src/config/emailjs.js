5bb099697b36ebac702da3f7f1a86b1ce65efdb5
Louan Flammanc <kusami.louan@gmail.com>
2025-10-06 21:18:30 -0400

    fix emailjs bug

---
+++ src/config/emailjs.js
@@ -0,0 +1,5 @@
+export const EMAILJS_CONFIG = {
+  SERVICE_ID: import.emailjs.env.EMAILJS_SERVICE_ID,
+  TEMPLATE_ID: import.emailjs.env.EMAILJS_TEMPLATE_ID,
+  PUBLIC_KEY: import.emailjs.env.EMAILJS_PUBLIC_KEY,
+};
